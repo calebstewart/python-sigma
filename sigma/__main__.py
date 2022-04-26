38a34434827669127ca992245148fb4142f1db05
Caleb Stewart <caleb.stewart94@gmail.com>
2022-04-26 11:55:36 -0400

    Bumped python version to 3.9
    Also ensured that entry_point usage was compatible with Python 3.9.
    Python 3.8 is not supported.

--- sigma/__main__.py
+++ sigma/__main__.py
@@ -6,7 +6,13 @@ from sigma import logger
 
 def entrypoint():
 
-    plugins = importlib.metadata.entry_points(group="sigma.cli")
+    # Fix compatability with pre-python-3.10 importlib
+    eps = importlib.metadata.entry_points()
+    if isinstance(eps, dict):
+        plugins = eps.get("sigma.cli", [])
+    else:
+        plugins = eps.select(group="sigma.cli")
+
     for plugin in plugins:
         try:
             plugin.load()
